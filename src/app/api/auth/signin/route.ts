fca9436f8a13299cd3c14d6d548b604aac3b4698
Siddharth <siddharth@aiimtech.in>
2024-09-03 16:27:13 +0530

    Signin endpoints

--- src/app/api/auth/signin/route.ts
+++ src/app/api/auth/signin/route.ts
@@ -1,18 +1,16 @@
 import { sql } from '@vercel/postgres';
 import { NextResponse } from 'next/server';
+import bcrypt from "bcrypt";
  
 export async function POST(request: Request) {
   const body = await request.json();
 
   try {
-    // const hashedPassword = await bcrypt.hash(body.password, 10);
-    // return NextResponse.json({ hashedPassword }, { status: 500 });
-    let res = {
-        "status":"success",
-        "msg":"login success"
-    };
-    return NextResponse.json({res}, { status: 200 });
-    await sql`INSERT INTO users (Name, password) VALUES (${body.name}, ${body.password});`;
+    const check = await sql`SELECT name from users WHERE name = ${body.name}`;
+    if(check.rows.length > 0) return NextResponse.json({"status":"failed", "errMsg":"Username already exists"}, { status: 400 });
+    const hashedPassword = await bcrypt.hash(body.password, 10);
+    await sql`INSERT INTO users (name, password) VALUES (${body.name}, ${hashedPassword});`;
+    return NextResponse.json({"status":"success", "msg":"login success"}, { status: 200 });
   } catch (error) {
     return NextResponse.json({ error }, { status: 500 });
   }
