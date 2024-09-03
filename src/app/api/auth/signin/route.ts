979574db5d662c149d7f92d9c333d6b302a1aeb4
Siddharth <siddharth@aiimtech.in>
2024-09-03 18:45:20 +0530

    validatin check in signin

--- src/app/api/auth/signin/route.ts
+++ src/app/api/auth/signin/route.ts
@@ -1,9 +1,22 @@
 import { sql } from '@vercel/postgres';
 import { NextResponse } from 'next/server';
 import bcrypt from "bcrypt";
+import { z } from "zod";
  
 export async function POST(request: Request) {
   const body = await request.json();
+
+  const schema = z.object({
+    name: z.string().min(6),
+    password: z.string().min(6)
+  });
+
+  const response = schema.safeParse(body);
+
+  if (!response.success) {
+    const { errors }  = response.error;
+    return NextResponse.json({error: { message: "Invalid request", errors }}, { status: 400 });
+  }
 
   try {
     const check = await sql`SELECT name from users WHERE name = ${body.name}`;
