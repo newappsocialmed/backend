4d17dc7334ccbb3cfbaeb3e6ce4104643c5fc9bb
Siddharth <siddharth@aiimtech.in>
2024-09-04 15:07:53 +0530

    push

--- src/app/api/auth/apik/route.ts
+++ src/app/api/auth/apik/route.ts
@@ -1,6 +1,4 @@
 import { NextResponse } from "next/server";
-import dotenv from 'dotenv';
-dotenv.config({ path: '.env.production' });
 
 export async function GET() {
     const apiK = process.env.API_KEY_SECRET;
