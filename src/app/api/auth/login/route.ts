8acaa30d83dae030942dd40da9a8c37ace3833ef
Siddharth <siddharth@aiimtech.in>
2024-09-04 18:55:52 +0530

    login changes

--- src/app/api/auth/login/route.ts
+++ src/app/api/auth/login/route.ts
@@ -26,21 +26,25 @@ export async function POST(request: Request) {
 
     try {
         const validuser = await sql`SELECT password, id from users WHERE name = ${body.name}`;
-        let hashPass = validuser.rows[0].password;
-        const isValidUser = await bcrypt.compare(body.password, hashPass);
-        if(isValidUser){
-            const jwtToken = process.env.JWT_TOKEN_SECRET ?? "";
-            const token = sign(
-                {
-                    id: validuser.rows[0].id,
-                    name: body.name
-                },
-                jwtToken
-            );
-            return NextResponse.json({"status":"success", "msg":"Login success", "jwtToken":token}, { status: 200 });
+        if(validuser.rows.length == 0)
+            return NextResponse.json({"status":"failed", "msg":"User Not Found"}, { status: 404 });
+        else{
+            let hashPass = validuser.rows[0].password;
+            const isValidUser = await bcrypt.compare(body.password, hashPass);
+            if(isValidUser){
+                const jwtToken = process.env.JWT_TOKEN_SECRET ?? "";
+                const token = sign(
+                    {
+                        id: validuser.rows[0].id,
+                        name: body.name
+                    },
+                    jwtToken
+                );
+                return NextResponse.json({"status":"success", "msg":"Login success", "jwtToken":token}, { status: 200 });
+            }
+            else
+                return NextResponse.json({"status":"failed", "msg":"Invalid User"}, { status: 404 });
         }
-        else
-            return NextResponse.json({"status":"failed", "msg":"Invalid User"}, { status: 200 });
     } catch (error) {
         return NextResponse.json({ error }, { status: 500 });
     }
