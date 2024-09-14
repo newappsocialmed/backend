106b43ccbc9c2e6b35aa9dadd60d2f9cf69d0192
Siddharth <siddharth@aiimtech.in>
2024-09-14 17:31:25 +0530

    backend updated

--- src/app/api/auth/signin/route.ts
+++ src/app/api/auth/signin/route.ts
@@ -7,11 +7,11 @@ export async function POST(request: Request) {
 
   try {
     const check = await sql`SELECT username from flutter WHERE username = ${body.name}`;
-    if(check.rows.length > 0) return NextResponse.json({"status":"failed", "errMsg":"Username already exists"}, { status: 200 });
+    if(check.rows.length > 0) return NextResponse.json({"status":"failed", "msg":"Username already exists"}, { status: 200 });
     const hashedPassword = await bcrypt.hash(body.password, 10);
     await sql`INSERT INTO flutter (username, password, value) VALUES (${body.name}, ${hashedPassword}, 0)`;
     return NextResponse.json({"status":"success", "msg":"signin success"}, { status: 200 });
   } catch (error) {
-    return NextResponse.json({ "status":"failed", "error":error }, { status: 500 });
+    return NextResponse.json({ "status":"failed", "msg":error }, { status: 500 });
   }
 }
