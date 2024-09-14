7555a02d1827ecb54f4262ababdd4392516b90fd
Siddharth <siddharth@aiimtech.in>
2024-09-14 16:50:26 +0530

    backend updated

--- src/app/api/screens/flutter/route.ts
+++ src/app/api/screens/flutter/route.ts
@@ -7,7 +7,7 @@ export async function POST(request: Request) {
         let value = await sql`SELECT value from flutter WHERE id = ${body.id};`;
         let updatedvalue = value.rows[0]['value'] + 1;
         await sql`UPDATE flutter SET value = ${updatedvalue} WHERE id = ${body.id};`;
-        return NextResponse.json({ "status":"success", "msg":"Value updated successfully" }, { status: 200 });
+        return NextResponse.json({ "status":"success", "msg":"Value updated successfully", "value":updatedvalue }, { status: 200 });
     } catch (error) {
         console.error('Error updating users:', error);
         return NextResponse.json({ error: error }, { status: 500 });
